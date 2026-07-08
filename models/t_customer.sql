a3f039efa17795c020720f7668906ab0ff18c7b3
Krishna K <krishnasnowflake26@gmail.com>
2026-07-08 03:31:17 +0000

    8th july

--- models/t_customer.sql
+++ models/t_customer.sql
@@ -1,7 +1,1 @@
-{{
-    config(
-        materialized='table',
-        transient=false
-    )
-}}
-select customer_id,concat(first_name,'_',last_name) as full_name from dev_Db.bronze.customers
+select customer_id,concat(first_name,'_',last_name) as full_name from {{ source('s1', 'customers') }}
